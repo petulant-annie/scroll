91396f24780e153e8aba557039bc566d0282a396
Anna <40352117+petulant-annie@users.noreply.github.com>
2019-05-01 18:34:19 +0300

    Add files via upload

--- scroll.jsx
+++ scroll.jsx
@@ -8,35 +8,64 @@ class Scroll extends React.Component {
     super(props);
     this.list = props.list;
     this.state = {
-      element: this.list.map((item, index) => <li className="li" key={index}>{item}</li>),
+      // element: [],
     };
-    this.handleScroll = this.handleScroll.bind(this);
+    // this.handleScroll = this.handleScroll.bind(this);
   }
 
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
+  // componentDidMount() {
+  //   window.addEventListener('scroll', this.handleScroll);
+  // }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener('scroll', this.handleScroll);
+  // }
+
+  // handleScroll() {
+  //   if (document.documentElement.clientHeight
+  //     + window.pageYOffset >= document.documentElement.scrollHeight) {
+  //     const element = this.state.element;
+  //     for (let i = 0; i < 40; i += 1) {
+  //       element.push(<div className="li" key={this.state.element.length + 1}>
+  //         <img src={this.state.element.avatar} alt="ava" />
+  //         <p>{this.state.element.name}</p>
+  //         <p>{this.state.element.email}</p>
+  //         {this.createButtons()}
+  //       </div>);
+  //     }
+  //     this.setState({ element });
+  //   }
+  // }
+
+  createButtons() {
+    return (
+      <div className="button-bar">
+        <button>Add</button>
+        <button>Remove</button>
+      </div>
+    );
   }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
+  createItem() {
+    const listItem = this.list.map(item =>
+      (
+        <div className="li">
+          <img src={item.avatar} alt="ava" />
+          <p>{item.name}</p>
+          <p>{item.email}</p>
+          {this.createButtons()}
+        </div>
+      ));
 
-  handleScroll() {
-    if (document.documentElement.clientHeight
-      + window.pageYOffset >= document.documentElement.scrollHeight) {
-      const element = this.state.element;
-      for (let i = 0; i < 40; i += 1) {
-        element.push(<li key={this.state.element.length + 1}>{this.list[i]}</li>);
-      }
-      this.setState({ element });
-    }
+
+    return (
+      listItem
+    );
   }
 
   render() {
     return (
-      <div id="listContainer">
-        {this.state.element}
-      </div>
+      this.createItem()
     );
   }
 }
