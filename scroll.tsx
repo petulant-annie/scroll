25506aa3bb99e94e01bc020a461882a8de43f399
Anna <40352117+petulant-annie@users.noreply.github.com>
2019-05-02 17:27:23 +0300

    Add files via upload

--- scroll.tsx
+++ scroll.tsx
@@ -17,24 +17,25 @@ interface IItem {
 class Scroll extends React.Component<IList> {
   containerRef: React.RefObject<HTMLDivElement>;
   list: JSX.Element[];
-  duplicateItem: any;
+  state: { duplicateItem: any };
+
   constructor(props: IList) {
     super(props);
     this.list = props.list;
     this.containerRef = React.createRef();
     this.handleScroll = this.handleScroll.bind(this);
-    this.duplicateItem = [];
+    this.state = {
+      duplicateItem: [],
+    };
   }
 
-  handleScroll() {
+  handleScroll = (e: React.UIEvent) => {
+    e.preventDefault();
     const current = this.containerRef.current;
     if (current.clientHeight + current.scrollTop >= current.scrollHeight) {
-      console.log(this.props.list);
-      for (let i = 0; i < this.list.length; i += 1) {
-        this.duplicateItem.push(this.props.list[i]);
-      }
+      this.setState({ duplicateItem: this.createItems });
+      console.log(this.state.duplicateItem);
     }
-    console.log(this.duplicateItem);
   }
 
   createButtons() {
@@ -46,26 +47,25 @@ class Scroll extends React.Component<IList> {
     );
   }
 
-  createItems() {
-    return (this.props.list as []).map((item: IItem, index: number) => (
-      <div key={index} className="li">
-        <img src={item.avatar} alt="ava" />
-        <p>{item.name}</p>
-        <p>{item.email}</p>
-        {this.createButtons()}
-      </div>
-    ));
-  }
+  createItems = (this.props.list as []).map((item: IItem, index: number) => (
+    <div key={index} className="li">
+      <img src={item.avatar} alt="ava" />
+      <p>{item.name}</p>
+      <p>{item.email}</p>
+      {this.createButtons()}
+    </div>
+  ));
 
   render() {
+    console.log(this.state.duplicateItem);
     return (
       <div
         className="listContainer"
         onScroll={this.handleScroll}
         ref={this.containerRef}
       >
-        {this.createItems()}
-        {this.duplicateItem}
+        {this.createItems}
+        {this.state.duplicateItem}
       </div>
     );
   }
