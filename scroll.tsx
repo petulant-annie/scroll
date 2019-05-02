a3da2fb2b210ea2d34fc5757c0d40e209bac703d
Anna <40352117+petulant-annie@users.noreply.github.com>
2019-05-02 16:44:09 +0300

    Add files via upload

--- scroll.tsx
+++ scroll.tsx
@@ -7,32 +7,31 @@ interface IList {
   list: JSX.Element[];
 }
 
+interface IItem {
+  avatar: string;
+  name: string;
+  email: string;
+  registration: number;
+}
+
 class Scroll extends React.Component<IList> {
   containerRef: React.RefObject<HTMLDivElement>;
   list: JSX.Element[];
-  duplicateItem: [];
+  duplicateItem: any;
   constructor(props: IList) {
     super(props);
     this.list = props.list;
-    this.duplicateItem = [];
     this.containerRef = React.createRef();
     this.handleScroll = this.handleScroll.bind(this);
+    this.duplicateItem = [];
   }
 
   handleScroll() {
     const current = this.containerRef.current;
-    if (current.clientHeight + current.scrollTop <= current.scrollHeight) {
-      const elem: JSX.Element[] = this.list.map((item, index) =>
-        (
-          <div key={index} className="li">
-            <img src={item.avatar} alt="ava" />
-            <p>{item.name}</p>
-            <p>{item.email}</p>
-            {this.createButtons()}
-          </div>
-        ));
+    if (current.clientHeight + current.scrollTop >= current.scrollHeight) {
+      console.log(this.props.list);
       for (let i = 0; i < this.list.length; i += 1) {
-        this.duplicateItem.push(elem[i]);
+        this.duplicateItem.push(this.props.list[i]);
       }
     }
     console.log(this.duplicateItem);
@@ -47,24 +46,25 @@ class Scroll extends React.Component<IList> {
     );
   }
 
-  render() {
-    const listItem: JSX.Element[] = this.list.map((item, index) =>
-      (
-        <div key={index} className="li">
-          <img src={item.avatar} alt="ava" />
-          <p>{item.name}</p>
-          <p>{item.email}</p>
-          {this.createButtons()}
-        </div>
-      ));
+  createItems() {
+    return (this.props.list as []).map((item: IItem, index: number) => (
+      <div key={index} className="li">
+        <img src={item.avatar} alt="ava" />
+        <p>{item.name}</p>
+        <p>{item.email}</p>
+        {this.createButtons()}
+      </div>
+    ));
+  }
 
+  render() {
     return (
       <div
         className="listContainer"
         onScroll={this.handleScroll}
         ref={this.containerRef}
       >
-        {listItem}
+        {this.createItems()}
         {this.duplicateItem}
       </div>
     );
